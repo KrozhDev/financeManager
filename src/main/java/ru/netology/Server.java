c8a93348dc1ab8e3c1d3db19f65959746bfff0b5
KrozhDev <roko.mdn@gmail.com>
2023-05-04 12:16:16 +0300

    calculations

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -10,11 +10,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
 
+import static ru.netology.Calculations.addTotalSumInfo;
+
 public class Server {
 
+    private static ObjectMapper objectMapper = new ObjectMapper();
     private static final int PORT = 8080;
-    private static HashMap<String,Integer> categoriesTotalSum = new HashMap<>();
-    private static final HashMap<String,ArrayList<String>> categories = tsvToHashMap();
+
 
     public static void main(String[] args) {
 
@@ -35,19 +37,12 @@ public class Server {
                     while (!"q".equals(jsonString)) {
                         jsonString = in.readLine();
                         if (!"q".equals(jsonString)){
-                            ObjectMapper objectMapper = new ObjectMapper();
                             BuyNote buyNote = objectMapper.readValue(jsonString, BuyNote.class);
                             addTotalSumInfo(buyNote);
                         }
-
                     }
-                    Map.Entry<String, Integer> maxEntry = categoriesTotalSum.entrySet().stream()
-                            .max(Comparator.comparing(Map.Entry::getValue)).orElse(null);
-                    JSONObject mainObject = getJsonObject(maxEntry);
+                    JSONObject mainObject = Calculations.getMaxCategoryReturnJsonObject();
                     out.println(mainObject.toString());
-
-
-
                 }
             }
         } catch (
@@ -57,61 +52,5 @@ public class Server {
         }
 
     }
-
-    private static JSONObject getJsonObject(Map.Entry<String, Integer> maxEntry) {
-        JSONObject maxCategory = new JSONObject();
-
-        maxCategory.put("category", maxEntry.getKey());
-        maxCategory.put("sum", maxEntry.getValue());
-
-        JSONArray maxMain = new JSONArray();
-        maxMain.put(maxCategory);
-
-        JSONObject mainObject = new JSONObject();
-        mainObject.put("maxCategory", maxMain);
-        return mainObject;
-    }
-
-    private static void addTotalSumInfo(BuyNote buyNote) {
-        String categ = "другое";
-        for (var elem: categories.entrySet()) {
-            if (elem.getValue().contains(buyNote.getTitle())) {
-                categ = elem.getKey();
-            }
-        }
-
-        if (categoriesTotalSum.containsKey(categ)) {
-            categoriesTotalSum.put(categ,categoriesTotalSum.get(categ)+ buyNote.getSum());
-        } else {
-            categoriesTotalSum.put(categ,buyNote.getSum());
-        }
-        categoriesTotalSum.entrySet().stream().forEach(System.out::println);
-    }
-
-    private static HashMap<String, ArrayList<String>> tsvToHashMap() {
-
-        final String dataFilePath = "categories.tsv";
-        HashMap<String,ArrayList<String>> categories = new HashMap<>();
-        String good;
-        String category;
-
-        TsvParserSettings settings = new TsvParserSettings();
-        com.univocity.parsers.tsv.TsvParser parser = new com.univocity.parsers.tsv.TsvParser(settings);
-        List<String[]> allRows = parser.parseAll(new File(dataFilePath),"UTF-8");
-
-        for (int i = 0; i < allRows.size(); i++){
-            good = Arrays.asList(allRows.get(i)).get(0);
-            category = Arrays.asList(allRows.get(i)).get(1);
-            if (categories.containsKey(category))  {
-                categories.get(category).add(good);
-            } else {
-                categories.put(category, new ArrayList<>() );
-                categories.get(category).add(good);
-            }
-        }
-        return categories;
-    }
-
-
-
 }
+
